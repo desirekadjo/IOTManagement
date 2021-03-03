2acbf5d66a02cbeb213d76b5853cf92ae703eadd
desirekadjo <63530641+desirekadjo@users.noreply.github.com>
2021-03-03 01:45:38 -0600

    Update JwtRequest.java
    added equals and HashCode annotations

--- iotdemo/model/JwtRequest.java
+++ iotdemo/model/JwtRequest.java
@@ -8,6 +8,7 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@EqualsAndHashCode
 public class JwtRequest implements Serializable {
 
 	private static final long serialVersionUID = 5926468583005150707L;
